dee71e00958d341ef598451b9fccc18e1b37e5d8
Max Voronov <max@maxvoronov.com>
2017-01-11 19:51:18 +0200

    added 4 template-components

---
+++ src/components/editor/editor.component.js
@@ -0,0 +1,20 @@
+
+import HeaderTemplate from '../template-components/header-template';
+import PicTextSectionTemplate from '../template-components/pic-text-section-template';
+import IconListTemplate from '../template-components/icon-list-template';
+import PicListTemplate from '../template-components/pic-list-template';
+
+export default  {
+  data: () => {
+    return {
+    }
+  },
+  methods : {
+  },
+  components: {
+    HeaderTemplate,
+    PicTextSectionTemplate,
+    IconListTemplate,
+    PicListTemplate
+  }
+}
---
+++ src/components/template-components/pic-list-template/index.vue
@@ -0,0 +1,5 @@
+<template src="./pic-list-template.component.html"></template>
+<script src="./pic-list-template.component.js"></script>
+<style src="./pic-list-template.component.scss" scoped lang="scss"></style>
+
+
