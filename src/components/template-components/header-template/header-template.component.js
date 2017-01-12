6661890a4109b0859b37c917a4ef0f31f71ee82e
Max Voronov <max@maxvoronov.com>
2017-01-12 13:25:21 +0200

    changed static text to variables

--- src/components/template-components/header-template/header-template.component.js
+++ src/components/template-components/header-template/header-template.component.js
@@ -2,7 +2,16 @@
 export default  {
   data: () => {
     return {
-      h1: "Hi, I'm "
+      h1_text_1: 'Hi, I\'m ',
+      h1_text_2_strong: 'Photon',
+      h1_text_3: ', another fine',
+      h1_a_href: 'http://html5up.net',
+      h1_a_text: 'HTML5 UP',
+      p_text_1: 'Accumsan feugiat mi commodo erat lorem ipsum, sed magna',
+      p_text_2: 'lobortis feugiat sapien sed etiam volutpat accumsan.',
+      li_a_text: 'Discover'
+
+
     }
   },
   methods : {
